66b0504e49f29c03f380cef76265d36cf0178a58
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2025-09-01 12:56:36 +0700

    update

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ makepkg -si --noconfirm
 cd ~
 
 echo "Installing packages..."
-yay -S --needed --noconfirm hyprland hyprpaper hypridle hyprlock xdg-desktop-portal-hyprland hyprpolkitagent uwsm libnewt dunst pipewire wireplumber qt5-wayland qt6-wayland inter-font ttf-jetbrains-mono noto-fonts ttf-noto-nerd waybar rofi-wayland cliphist nautilus xdg-desktop-portal-gtk wiremix grim slurp imagemagick wayfreeze-git brightnessctl jq gvfs wget tree man-db nodejs-lts-jod npm jdk-openjdk php apache php-apache mariadb alacritty htop blueberry visual-studio-code-bin firefox baobab decibels gnome-calculator gnome-calendar gnome-clocks gnome-disk-utility gnome-maps gnome-music gnome-text-editor gnome-weather loupe papers showtime snapshot sushi file-roller
+yay -S --needed --noconfirm hyprland hyprpaper hypridle hyprlock xdg-desktop-portal-hyprland hyprpolkitagent uwsm libnewt dunst pipewire wireplumber qt5-wayland qt6-wayland inter-font ttf-jetbrains-mono noto-fonts ttf-noto-nerd waybar rofi-wayland cliphist nautilus xdg-desktop-portal-gtk wiremix grim slurp imagemagick wayfreeze-git brightnessctl jq gvfs wget tree man-db nodejs-lts-jod npm jdk-openjdk php apache php-apache mariadb alacritty htop blueberry visual-studio-code-bin firefox baobab decibels evince gnome-calculator gnome-calendar gnome-clocks gnome-disk-utility gnome-maps gnome-music gnome-text-editor gnome-weather loupe snapshot sushi totem file-roller
 
 yay -S --needed --noconfirm --asdeps pipewire-pulse gvfs-mtp noto-fonts-cjk noto-fonts-emoji noto-fonts-extra arj binutils bzip3 cdrtools cpio dpkg lhasa lrzip 7zip rpmextract squashfs-tools unace unrar unzip zip
 
