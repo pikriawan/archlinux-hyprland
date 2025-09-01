84e2b168cdf3541bf65579b5045ea7a812634ca2
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2025-09-01 14:47:29 +0700

    update

--- push.sh
+++ push.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ -z "$@" ]; then
+    echo "No commit message. Push aborted"
+    exit 1
+fi
+
 rm .config.tar.xz
 rm bin.tar.xz
 tar -cf .config.tar.xz .config/
@@ -6,3 +11,4 @@ tar -cf bin.tar.xz bin/
 git add .
 git commit -m "$@"
 git push
+echo "Push successfull"
