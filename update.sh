8a0a4ee1047a0cbd48850f3e8ed4c76e142e74de
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2025-09-02 18:48:43 +0700

    update

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Copying configurations..."
+curl -O https://raw.githubusercontent.com/pikriawan/archlinux-hyprland/refs/heads/main/.config.tar.xz
+tar -xf .config.tar.xz
+rm .config.tar.xz
+
+sed -i "s|@HOME@|$HOME|g" .config/hypr/hyprpaper.conf
+sed -i "s|@HOME@|$HOME|g" .config/hypr/hyprlock.conf
+sed -i "s|@HOME@|$HOME|g" .config/waybar/style.css
+
+echo "Copying executables..."
+curl -O https://raw.githubusercontent.com/pikriawan/archlinux-hyprland/refs/heads/main/bin.tar.xz
+tar -xf .local.tar.xz
+rm .local.tar.xz
